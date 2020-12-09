bf5833b86301ade0e0690a4f874524a9ffa0ccfc
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-12-08 21:18:43 -0700

    implemented Multiply

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,7 @@ import sys
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -21,28 +22,24 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
-        # For now, we've just hardcoded a program:
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+       # open the file
+        with open(filename) as my_file:
+           for line in my_file:
+                comment_split = line.split("#")
+                maybe_binary_number = comment_split[0]
+                try:
+                    x = int(maybe_binary_number, 2)
+                    self.ram_write(x, address)
+                    address += 1
+                except:
+                    continue 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        if op == MUL:
+            self.registers[reg_a] *= self.registers[reg_b]
+            self.pc += 3
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -99,5 +96,7 @@ class CPU:
         elif instruction == LDI:
             self.registers[opperand_a] = opperand_b
             self.pc += 3
+        elif instruction == MUL:
+            self.alu(instruction, opperand_a, opperand_b)
         else:
             print('I dont know what to do.')
